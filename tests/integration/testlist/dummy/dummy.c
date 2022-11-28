95c3e8e2e67b4f939cf8b27f44b83a32ffb5faa5
pcdeni <pcdeni@gmail.com>
2022-11-28 16:08:38 +0100

    test

--- tests/integration/testlist/dummy/dummy.c
+++ tests/integration/testlist/dummy/dummy.c
@@ -7,9 +7,9 @@ int dummy()
   uint64_t mhartid;
   asm volatile ("csrr %[reg], mhartid" : [reg] "=r" (mhartid));
 
-  if(mhartid==1)
-    return 0;
   if(mhartid==0)
+    return 0;
+  if(mhartid==1)
     { asm volatile ("wfi"); }
 
     return 1;
