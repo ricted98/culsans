4bdfff39f94748861a16d504735d747f84198ebd
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-02-09 16:56:40 +0100

    Adjust cache specs

--- tests/performance/inc/cache.h
+++ tests/performance/inc/cache.h
@@ -2,13 +2,12 @@
 #define __CACHE_H__
 
 #define CACHE_WAYS 8
-#define CACHE_ENTRIES 128
+#define CACHE_ENTRIES 256
 #define CACHE_LINE_BYTE 16
-#define CACHE_SIZE_BYTE 16384
-#define NUM_WORDS 11
+#define CACHE_SIZE_BYTE 32768
 
 // cachelines are 128bit long
-// cache is 16kB: 16B cachelines x 128 entries x 8 ways
+// cache is 32kB: 16B cachelines x 256 entries x 8 ways
 #define cacheline_t __uint128_t
 
 #endif
